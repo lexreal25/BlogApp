d508ca6989e7cd2d349c6adc3d842a2a160de795
Daniel Nagai <daniel@shop-office.net>
2021-05-11 02:38:43 +0000

    added blog sections

--- App.js
+++ App.js
@@ -1,24 +1,54 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native'
-import { createDrawerNavigator } from '@react-navigation/drawer'
-import { createStackNavigator } from '@react-navigation/stack'
+import { AntDesign } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
+// import { createDrawerNavigator } from '@react-navigation/drawer'
+//import { createStackNavigator } from '@react-navigation/stack'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+
 
 import Discover from './components/Discover';
-import Card from './components/Card'
 import Beauty from './components/Beauty'
-const Drawer = createDrawerNavigator()
+import Settings from './components/Settings'
+import Account from './components/Account'
+
+//const Drawer = createDrawerNavigator()
 
 //const Stack = createStackNavigator()
 
+const Tab = createBottomTabNavigator()
 
 export default function App() {
   return (
     <NavigationContainer style={styles.container}>
-      <Drawer.Navigator initialRouteName="Discover" screenOptions={{headerShown:true}}>
-        <Drawer.Screen name='Discover' component={Discover}  />
-        <Drawer.Screen name="Beauty" component={Beauty}/>
-      </Drawer.Navigator>
+      <Tab.Navigator initialRouteName="Discover" 
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({focused, color, size}) => {
+            let iconName;
+            if(route.name === "Discover"){
+              iconName = focused ? 'home' : 'home-outline';
+            }else if(route.name === 'Beauty'){
+              iconName = focused ? 'folder-sharp' :'folder-outline'
+            }else if(route.iconName === "Settings"){
+              iconName = focused ? 'settings' : 'settings-outline'
+            }
+            else if(route.iconName === "Account"){
+              iconName = focused ? 'md-person' : 'md-person-outline'
+            }
+             return <Ionicons name={iconName} size={size} color={color} />
+          },
+        })}
+          tabBarOptions={{
+            activeTintColor: 'purple',
+            inactiveTintColor: 'gray'
+          }}
+        >
+        <Tab.Screen name='Discover' component={Discover}  />
+        <Tab.Screen name="Beauty" component={Beauty}/>
+        <Tab.Screen name="Account" component={Account}/>
+        <Tab.Screen name="Settings" component={Settings}/>
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
@@ -30,3 +60,5 @@ const styles = StyleSheet.create({
     
   },
 });
+
+
--- components/Beauty.jsx
+++ components/Beauty.jsx
@@ -1,13 +1,44 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Image, ScrollView } from 'react-native'
+
+import { AntDesign } from '@expo/vector-icons';
+import { Feather } from '@expo/vector-icons';
+
+
+
 
 export default function Beauty({route, navigation}) {
 
     const {title,coverImage,profileImage,profileName,time,details} = route.params
 
     return (
-        <View>
-            <Text>{title}</Text>
+        <ScrollView>
+        <View style={{flex: 1}}>
+            <View style={{width: 250, height: 250, backgroundColor:'pink'}}></View>
+            <View style={{flex:1, padding: 25,position:'absolute', top: 30,}}>
+            {/* header icons */}
+            <View style={{flexDirection:'row',marginVertical: 10, justifyContent:'space-between'}}>
+                <View style={{flexDirection:'row', justifyContent:'space-between'}}>
+                    <AntDesign name="left" size={25} color="black" />
+                    <Text style={{fontSize:20, fontWeight:'700'}}>Beauty</Text>
+                </View>
+                <View style={{flexDirection:'row', justifyContent:'space-between'}}>
+                    <Feather name="headphones" size={20} color="black" />
+                    <AntDesign name="hearto" size={20} color="black" style={{marginLeft:5}}/>
+                    <AntDesign name="sharealt" size={20} color="black" style={{marginLeft:5}}/>
+                </View>
+            </View>
+
+                <Image source={ coverImage } style={{width: '100%', height: 250}} />
+                <Text style={{fontSize: 20,fontWeight: 700, marginVertical:10}}>{title}</Text>
+                <View style={{flexDirection:'row',marginVertical: 10}}>
+                    <Image source={profileImage} style={{width: 20, height: 20, borderRadius:50}} />
+                    <Text style={{marginLeft: 5}}>{profileName}</Text>
+                    <Text style={{marginLeft:5, color:'#D2D7D3'}}>{time}</Text>
+                </View>
+                <Text style={{fontSize: 17, lineHeight: 30, marginVertical: 10}}>{details}</Text>
+            </View>
         </View>
+        </ScrollView>
     )
 }
